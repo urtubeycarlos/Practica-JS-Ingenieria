8405db0c8d7be3610d2ae5795b807d3d797bef7e
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2017-09-29 01:39:51 -0300

    First commit.

--- js/tests.js
+++ js/tests.js
@@ -23,10 +23,11 @@ QUnit.test("obtenerParesTest", function(assert){
 
 QUnit.test("obtenerImparesTest", function(assert){
 	
+	var res_esperados = [1, 3, 9];
 	resultado = procesarElementosLista(lista, PROCESOS_ESTANDAR.obtenerImpares);
-	assert.equal(resultado[0], 1);
-	assert.equal(resultado[1], 3);
-	assert.equal(resultado[2], 9);
+	
+	for (var i = 0; i < resultado.length; i++)
+		assert.equal(resultado[i], res_esperados[i]);
 
 });
 
@@ -49,7 +50,7 @@ QUnit.test("modificarTest", function(assert){
 	var res_esperados = [2, 4, 6, 16, 18, 20];
 	resultado = procesarElementosLista(lista, PROCESOS_ESTANDAR.modificar, elem => elem*2);
 	
-	for (var i = resultado.length - 1; i >= 0; i--)
+	for (var i = 0; i < resultado.length; i++)
 		assert.equal(resultado[i], res_esperados[i]);
 
 });
@@ -57,7 +58,7 @@ QUnit.test("modificarTest", function(assert){
 QUnit.test("filtrarTest", function(assert){
 
 	var res_esperados = [1, 2, 3];
-	resultado = procesarElementosLista(lista, PROCESOS_ESTANDAR.filtrar, elem => elem<4);
+	resultado = procesarElementosLista(lista, PROCESOS_ESTANDAR.filtrar, elem => elem < 4);
 	
 	for (var i = resultado.length - 1; i >= 0; i--)
 		assert.equal(resultado[i], res_esperados[i]);
@@ -68,12 +69,12 @@ QUnit.test("acumularTest", function(assert){
 
 	var multiplicacion = procesarElementosLista(lista, PROCESOS_ESTANDAR.acumular, (acum, elem) => {
 		acum = acum || 1;
-		return acum*elem
+		return acum * elem;
 	});
 
 	var suma = procesarElementosLista(lista, PROCESOS_ESTANDAR.acumular, (acum, elem) => {
 		acum = acum || 0;
-		return acum + elem
+		return acum + elem;
 	});
 
 	assert.ok(4320 == multiplicacion);
@@ -81,5 +82,17 @@ QUnit.test("acumularTest", function(assert){
 
 });
 
+QUnit.test("multiplesProcesosTest", function(assert){
 
+	var nuevaLista = procesarElementosLista(lista, PROCESOS_ESTANDAR.modificar, elem => elem + 4);
+	console.log(nuevaLista);
+	nuevaLista = procesarElementosLista(nuevaLista, PROCESOS_ESTANDAR.filtrar, elem => elem > 7);
+	var sumaLista = procesarElementosLista(nuevaLista, PROCESOS_ESTANDAR.acumular, (acum, elem) => {
+		acum = acum || 0;
+		return acum + elem;
+	});
+
+	assert.equal(39, sumaLista);
+
+});
 
